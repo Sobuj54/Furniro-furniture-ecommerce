08d07e30a298e53e1aff7378aeb71f2c8217010b
soubj <sobujahmed124@gmail.com>
2024-01-30 23:54:01 +0600

    made footer responsive

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
       <div className="w-full h-[1px] border-slate-300 border-[1px]" />
 
       <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-12 gap-7 mt-14">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-10 md:gap-20 px-10 lg:px-0 lg:gap-7 mt-14">
           {/* logo span */}
-          <div className="col-span-4">
+          <div className="lg:col-span-4 order-1">
             <Link to="/" className="flex gap-1">
               <img className="w-9" src="/favicon.svg" alt="icon" />
               <h3 className="text-2xl font-bold font-montserrat">Furniro</h3>
@@ -19,7 +19,7 @@ const Footer = () => {
             </p>
           </div>
           {/* links span */}
-          <div className="col-span-2 flex flex-col gap-10 font-medium">
+          <div className="lg:col-span-2 order-3 lg:order-2 flex flex-col gap-5 md:gap-10 font-medium">
             <h6 className="text-base text-black/60">Links</h6>
             <Link to="/">Home</Link>
             <Link to="/shop">Shop</Link>
@@ -28,7 +28,7 @@ const Footer = () => {
           </div>
 
           {/* help span */}
-          <div className="col-span-2 flex flex-col gap-10 font-medium">
+          <div className="lg:col-span-2 order-4 lg:order-3 flex flex-col gap-5 md:gap-10 font-medium">
             <h6 className="text-base text-black/60">Help</h6>
             <Link to="/">Payment Options</Link>
             <Link to="/shop">Returns</Link>
@@ -36,7 +36,7 @@ const Footer = () => {
           </div>
 
           {/* newsletter span */}
-          <div className="col-span-4 flex flex-col gap-10 font-medium">
+          <div className="lg:col-span-4 order-2 lg:order-4 flex flex-col gap-5 md:gap-10 font-medium">
             <h6 className="text-base text-black/60">Newsletter</h6>
             <form>
               <input
@@ -52,7 +52,9 @@ const Footer = () => {
         </div>
 
         <div className="w-full h-[1px] border-slate-300 border-[1px] mt-10" />
-        <p className="my-10 font-medium">2024 furniro. All rights reserved</p>
+        <p className="my-10 font-medium px-10 lg:px-0">
+          2024 furniro. All rights reserved
+        </p>
       </div>
     </section>
   );
